4b2a05bb5c016d19c9ed53d331f319455ab94991
Gerrit Boesebeck <boesebeck@e-spirit.com>
2016-09-09 13:38:39 +0200

    DEVEX-43 Fixed for whitespaces in projectnames

--- fsdevtools-cli/src/main/archive/fs-cli.sh
+++ fsdevtools-cli/src/main/archive/fs-cli.sh
@@ -2,6 +2,6 @@
 JAVACMD="$JAVA_HOME/bin/java";
 FS_CLI_DIR="$(dirname $(readlink -f $0))/../";
 JARFILE="${FS_CLI_DIR}lib/${project.artifactId}-${project.version}.jar";
-$JAVACMD -Dlog4j.configuration=file:"${FS_CLI_DIR}conf/log4j.properties" -jar $JARFILE $@;
+$JAVACMD -Dlog4j.configuration=file:"${FS_CLI_DIR}conf/log4j.properties" -jar $JARFILE "$@";
 RETVAL=$?;
 exit ${RETVAL};
