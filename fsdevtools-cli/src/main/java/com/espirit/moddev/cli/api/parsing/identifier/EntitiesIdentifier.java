84ef639f33c1bcc24a7ff63a00597df8f97e5c81
Hannes Pernpeintner <pernpeintner@e-spirit.com>
2017-02-17 16:27:03 +0100

    DEVEX-65 added documentation and license header

--- fsdevtools-cli/src/main/java/com/espirit/moddev/cli/api/parsing/identifier/EntitiesIdentifier.java
+++ fsdevtools-cli/src/main/java/com/espirit/moddev/cli/api/parsing/identifier/EntitiesIdentifier.java
@@ -1,3 +1,25 @@
+/*
+ *
+ * *********************************************************************
+ * fsdevtools
+ * %%
+ * Copyright (C) 2016 e-Spirit AG
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * *********************************************************************
+ *
+ */
+
 package com.espirit.moddev.cli.api.parsing.identifier;
 
 import com.google.common.base.Strings;
@@ -10,11 +32,20 @@ import de.espirit.firstspirit.agency.StoreAgent;
 import de.espirit.firstspirit.store.access.nexport.operations.ExportOperation;
 import org.slf4j.LoggerFactory;
 
+/**
+ * Identifier for FirstSpirit datasources. The uid parameter of the Content2 object is
+ * used to reference datasources.
+ */
 public class EntitiesIdentifier implements Identifier {
     protected static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(EntitiesIdentifier.class);
 
     private final String uid;
 
+    /**
+     * Instantiates a new identifier for entities.
+     * @throws IllegalArgumentException if a null or empty string is passed as uid
+     * @param uid the uid of the Content2 object
+     */
     public EntitiesIdentifier(String uid) {
         if(Strings.isNullOrEmpty(uid)) {
             throw new IllegalArgumentException("Don't pass an empty or null uid to content2 identifier!");
--- fsdevtools-cli/src/main/java/com/espirit/moddev/cli/api/parsing/parser/EntitiesIdentifierParser.java
+++ fsdevtools-cli/src/main/java/com/espirit/moddev/cli/api/parsing/parser/EntitiesIdentifierParser.java
@@ -1,3 +1,25 @@
+/*
+ *
+ * *********************************************************************
+ * fsdevtools
+ * %%
+ * Copyright (C) 2016 e-Spirit AG
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * *********************************************************************
+ *
+ */
+
 package com.espirit.moddev.cli.api.parsing.parser;
 
 import com.espirit.moddev.cli.api.parsing.identifier.EntitiesIdentifier;
@@ -7,6 +29,12 @@ import org.slf4j.LoggerFactory;
 import java.util.*;
 import java.util.regex.Pattern;
 
+/**
+ * Parser implementation that is able to parse FirstSpirit Content2 identifiers from a list of strings
+ * and return a list of entity identifier instances.
+ * Is applicable to strings of the form "entities:news" with "entities" as a prefix, ":" as a delimiter
+ * and a Content2 uid as a postfix.
+ */
 public class EntitiesIdentifierParser  implements Parser<EntitiesIdentifier> {
     private static final Pattern DELIMITER = Pattern.compile("\\s*:\\s*");
 
