4bc7dd10df85dd1feb3d53437e9dd3fd9f9c8625
Pernpeintner, Hannes <pernpeintner@e-spirit.com>
2017-09-27 15:59:22 +0200

    Merge pull request #25 in DEVEX/fsdevtools from DEVEX-118_xmx to master
    Squashed commit of the following:
    
    commit 0bec13178065d8b66fe390193d4431a112dc76f6
    Author: Sebastian Gockel <gockel@e-spirit.com>
    Date:   Wed Sep 27 15:35:44 2017 +0200
    
        DEVEX-118 - added -Xmx=512m param

--- fsdevtools-cli/src/main/archive/fs-cli.sh
+++ fsdevtools-cli/src/main/archive/fs-cli.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 JAVACMD="$JAVA_HOME/bin/java";
 FS_CLI_DIR="$(dirname $(readlink -f $0))/../";
-$JAVACMD -Dlog4j.configuration=file:"${FS_CLI_DIR}conf/log4j.properties" -cp "${FS_CLI_DIR}/lib/*" com.espirit.moddev.cli.Cli "$@";
+$JAVACMD -Xmx512m -Dlog4j.configuration=file:"${FS_CLI_DIR}conf/log4j.properties" -cp "${FS_CLI_DIR}/lib/*" com.espirit.moddev.cli.Cli "$@";
 RETVAL=$?;
 exit ${RETVAL};
 
