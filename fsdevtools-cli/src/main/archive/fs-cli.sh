40506a3a11ed5fd44ecf6218a99c84843ea5deaa
Tenter <tenter@e-spirit.com>
2017-09-28 14:50:14 +0200

    Merge pull request #26 in DEVEX/fsdevtools from DEVEX-95_mac_os_support to master
    Squashed commit of the following:
    
    commit 4aefc6d3da267eadf00191725b1d6ea991e14b6d
    Author: Sebastian Gockel <gockel@e-spirit.com>
    Date:   Thu Sep 28 14:31:20 2017 +0200
    
        DEVEX-95 - fix cli dir definition for mac os

--- fsdevtools-cli/src/main/archive/fs-cli.sh
+++ fsdevtools-cli/src/main/archive/fs-cli.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 JAVACMD="$JAVA_HOME/bin/java";
-FS_CLI_DIR="$(dirname $(readlink -f $0))/../";
+FS_CLI_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../";
 $JAVACMD -Xmx512m -Dlog4j.configuration=file:"${FS_CLI_DIR}conf/log4j.properties" -cp "${FS_CLI_DIR}/lib/*" com.espirit.moddev.cli.Cli "$@";
 RETVAL=$?;
 exit ${RETVAL};
